1062b218f0a3e34c49884f63b80caf61adef3ffd
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-28 00:50:51 -0700

    Now nyc will use the non-transpiled version

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import domEvents from "./"
+import domEvents from "./index"
 
 const dom = {
   events (event) {
