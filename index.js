adbe8b163ac6ea70ed235c7ac68ebc726a13356e
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:28 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-export default function domEvents (options: DOMEventsConfigurationType): Function {
-  return function domEventsOptions (name: DOMEventNameType): Function {
-    return function domEventsOptionsName (dom: DOMStreamType): DOMEventStreamType {
-      return dom.events(name, options)
-    }
-  }
+export default function domEvents(options) {
+  return function domEventsOptions(name) {
+    return function domEventsOptionsName(dom) {
+      return dom.events(name, options);
+    };
+  };
 }
